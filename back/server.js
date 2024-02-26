ef70d44f26679bbbfe3405bbae8a73bb46796c15
nuuye <thomassmostowfi@gmail.com>
2024-02-26 21:05:43 +0100

    project init

---
+++ back/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+// app variable to configure the server
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Connect to MongoDB
+// We put the connection string in the .env file
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+// Log errors and open connection
+db.on('error', (error) => console.error('Error connecting to MongoDB:', error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+
+app.use(express.json());
+// Import routes
+const usersRouter = require('./routes/register');
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.use('/register', usersRouter);
+// Start server on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+}); 
+
+module.exports = app;
